8654b743713f67e28072b593d1c4de5b3b1f2819
George <gwromano@uark.edu>
2022-03-22 21:35:31 -0500

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,8 +6,11 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return HttpResponse('Hello from Python!')
+    # r = requests.get('https://httpbin.org/status/418')
+    # print(r.text)
+    # return HttpResponse('<pre>' + r.text + '</pre>')
+    #return render(request, "index.html")
 
 
 def db(request):
