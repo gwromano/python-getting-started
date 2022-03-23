2256fdc70a7160108d4dd470b46c88fd2866dcd7
George <gwromano@uark.edu>
2022-03-22 21:44:58 -0500

    edited

--- hello/views.py
+++ hello/views.py
@@ -6,11 +6,11 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    #return HttpResponse('Hello from Python!')
     # r = requests.get('https://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
-    #return render(request, "index.html")
+    return render(request, "index.html")
 
 
 def db(request):
